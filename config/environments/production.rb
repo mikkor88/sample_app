472616276adbab9749660bdda03d3dbd24989b13
Mikko Ruuskanen <mikko.ruuskanen@mbnet.fi>
2011-10-22 16:33:34 +0300

    precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,6 +16,8 @@ SampleApp::Application.configure do
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
+  
+  config.assets.precompile += %w( *.css *.js )
 
   # Generate digests for assets URLs
   config.assets.digest = true
