0473ff883c2f4b51ea63b77289a77d9e86041869
Mikko Ruuskanen <mikko.ruuskanen@mbnet.fi>
2011-10-22 16:41:13 +0300

    precompile2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ SampleApp::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
   
   config.assets.precompile += %w( *.css *.js )
 
