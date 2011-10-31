0a0f8872530af33d0805cb3454713fec87a42674
Mikko Ruuskanen <mikko.ruuskanen@mbnet.fi>
2011-10-31 20:24:18 +0200

    Done with user edit/update, index and destroy actions

--- Gemfile
+++ Gemfile
@@ -7,10 +7,12 @@ gem 'rails', '3.1.0'
 
 gem 'pg'
 gem 'gravatar_image_tag', '1.0.0.pre2'
+gem 'will_paginate', '3.0'
 
 group :development do
   gem 'rspec-rails', '2.6.1'
   gem 'annotate', :git => 'git://github.com/jeremyolliver/annotate_models.git', :branch => 'rake_compatibility'
+	gem 'faker', '0.3.1'
 end
 
 group :test do
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -99,45 +99,65 @@ describe User do
   
   describe "password encryption" do
   
-    before(:each) do
-	  @user = User.create!(@attr)
-	end
+		before(:each) do
+				@user = User.create!(@attr)
+		end
 	
-	it "should have an encrypted password" do
-	 @user.should respond_to(:encrypted_password)
-	end
+			it "should have an encrypted password" do
+				@user.should respond_to(:encrypted_password)
+			end
 	
-	it "should set the encrypted password" do
-	  @user.encrypted_password.should_not be_blank
-	end
+			it "should set the encrypted password" do
+				@user.encrypted_password.should_not be_blank
+			end
 	
-	describe "has_password? method" do
+		describe "has_password? method" do
 	
-	  it "should be true if the passwords match" do
-	    @user.has_password?(@attr[:password]).should be_true
-	  end
+			it "should be true if the passwords match" do
+				@user.has_password?(@attr[:password]).should be_true
+			end
 	  
-	  it "should be false if the passwords do not match" do
-	    @user.has_password?("invalid").should be_false
-	  end
-	end
+			it "should be false if the passwords do not match" do
+				@user.has_password?("invalid").should be_false
+			end
+		end
 	
-	describe "authenticate method" do
+		describe "authenticate method" do
 	  
-	  it "should return nil on email/password mismatch" do
-	    wrong_password_user = User.authenticate(@attr[:email], "wrongpass")
-		wrong_password_user.should be_nil
-	  end
+			it "should return nil on email/password mismatch" do
+				wrong_password_user = User.authenticate(@attr[:email], "wrongpass")
+				wrong_password_user.should be_nil
+			end
 	  
-	  it "should return nil an email address with no user" do
-	    nonexistent_user = User.authenticate("bar@foo.com", @attr[:password])
-		nonexistent_user.should be_nil
-	  end
+			it "should return nil an email address with no user" do
+				nonexistent_user = User.authenticate("bar@foo.com", @attr[:password])
+				nonexistent_user.should be_nil
+			end
 	  
-	   it "should return the user on email/password match" do
-	    matching_user = User.authenticate(@attr[:email], @attr[:password])
-		matching_user.should == @user
-	  end
-	end
+			it "should return the user on email/password match" do
+				matching_user = User.authenticate(@attr[:email], @attr[:password])
+				matching_user.should == @user
+			end
+		end
   end
+	
+	describe "admin attribute" do
+	
+		before(:each) do
+			@user = User.create!(@attr)
+		end
+		
+		it "should respond to admin" do
+			@user.should respond_to(:admin)
+		end
+		
+		it "should not be an admin by default" do
+			@user.should_not be_admin
+		end
+		
+		it "should be convertible to an admin" do
+			@user.toggle!(:admin)
+			@user.should be_admin
+		end
+	end	
 end
